8e10419a297611c5ee672293019dfd70bd4f5290
北村旭 <kurukurumoca@gmail.com>
2017-01-14 16:49:41 +0900

    Revert "Add comment"
    This reverts commit 3ae4c4d0b87a8fcb5039a56c60fb9207dea54366.

--- Titanic/pre_process.py
+++ Titanic/pre_process.py
@@ -22,29 +22,27 @@ BINARIZED_CAT_DICT = {"Sex":      ["male", "female"],
                       }
 
 
-# cannot understand
-
 def load_titanic_csv(csv_file_path):
     """
     Import a Titanic csv file as Pandas DataFrame instance.
     Parameters:
         csv_file_path: file path of data for Kaggle Titanic Compe
     Returns:
-        raw_df:
+        raw_df: 
     """
     raw_df = pd.read_csv(csv_file_path, header=0, index_col="PassengerId")
     if not "Survived" in raw_df.keys():
         raw_df["Survived"] = np.NaN
-
+    
     return raw_df
 
-
+    
 def txt_w_border(txt):
     BORDER_CHARS = 15 * "#"
     ret_txt = " ".join(["\n", BORDER_CHARS, txt, BORDER_CHARS])
     return ret_txt
-
-
+    
+    
 def print_info(df, start=0, end=4, details=1):
     """
     Print some information of the input dataframe.
@@ -57,11 +55,11 @@ def print_info(df, start=0, end=4, details=1):
     """
     print txt_w_border("Info")
     print df.info()
-
+    
     if details>1:
         print txt_w_border("Statistics")
         print df.describe(percentiles=[])
-
+    
     if details>2:
         try:
             sample_df = df[start:end].copy()
@@ -70,10 +68,10 @@ def print_info(df, start=0, end=4, details=1):
             sample_df = df.head()
             print txt_w_border("Samples, df.head")
         print sample_df
-
+    
     return None
 
-
+    
 def filter_cols(df):
     """
     Pick up by keyword list.
@@ -85,10 +83,10 @@ def filter_cols(df):
     """
     comm_keys = list( set(df.keys()) & set(KEYS_FOR_ML) )
     filt_col_df = df.copy()[comm_keys]
-
+    
     return filt_col_df
-
-
+    
+    
 def binarize(df):
     """
     Binarize categorial dataframe.
@@ -103,12 +101,12 @@ def binarize(df):
         val = BINARIZED_CAT_DICT[key]
         ret_df[key] = ret_df[key].astype("category")
         ret_df[key] = ret_df[key].cat.set_categories(val)
-
+        
     ret_df = pd.get_dummies(ret_df, columns=comm_keys, drop_first=True).astype("float")
-
+    
     return ret_df
 
-
+    
 def normalize(df):
     """
     Nomarize dataframe without NaN.
@@ -121,24 +119,24 @@ def normalize(df):
         out_df: Nomarized dataframe
     """
     comm_keys = list( set(df.keys()) & set(KEYS_FOR_NORM) )
-
+    
     ret_df = df.copy()
     t = ret_df[comm_keys]
     ret_df[comm_keys] = (t - t.mean()) / t.std()
-
+    
     return ret_df
-
+    
 
 def add_null_flag_cols(df, del_single_cat_cols=False):
     keys_of_null_cols = [ k + "_null" for k in df.keys()]
-
+                         
     ret_df = df.copy()
     ret_df[keys_of_null_cols] = ret_df[df.keys()].isnull()
     if del_single_cat_cols:
         ret_df = pd.get_dummies(ret_df, columns=keys_of_null_cols, drop_first=True)
-
+    
     return ret_df
-
+    
 
 def merge_SibSp_Parch_to_FamSize(df):
     ret_df = df.copy()
@@ -146,42 +144,42 @@ def merge_SibSp_Parch_to_FamSize(df):
     ret_df.drop(["SibSp","Parch"], axis=1, inplace=True)
     return ret_df
 
-
+    
 def pre_proc_per_df(df, del_single_cat_cols=False):
     ret_df = df.copy()
-
+    
 #    print txt_w_border("Merging SibSp and Parch to FamSize")
 #    ret_df = merge_SibSp_Parch_to_FamSize(ret_df)
-
+    
     print txt_w_border("Filtering " + str(KEYS_FOR_ML))
     ret_df = filter_cols(ret_df)
-
+    
     print txt_w_border("Adding null flag columns")
     ret_df = add_null_flag_cols(ret_df, del_single_cat_cols)
-
+    
     print txt_w_border("Binarizing " + str(BINARIZED_CAT_DICT.keys()))
     ret_df = binarize(ret_df)
-
+    
     print txt_w_border("Nomarizing " + str(KEYS_FOR_NORM))
     ret_df = normalize(ret_df)
-
+    
     key_of_fill = 0.
     print txt_w_border("Filling null with " + str(key_of_fill))
     ret_df = ret_df.fillna(key_of_fill)
-
+    
     return ret_df
 
 
 def pre_proc_all():
     print txt_w_border("Importing " + FILE_PATH_TRAIN_DATA)
     raw_train_df = load_titanic_csv(FILE_PATH_TRAIN_DATA)
-
+    
     print txt_w_border("Importing " + FILE_PATH_PREDICT_DATA)
     raw_test_df = load_titanic_csv(FILE_PATH_PREDICT_DATA)
-
+    
     raw_all_df  = pd.concat([raw_train_df, raw_test_df])
     t_df = pre_proc_per_df(raw_all_df, del_single_cat_cols=True)
-
+    
     def split_train_target(df, tf_surv):
         temp_df = df[df.Survived_null_True==tf_surv]
         target_df = temp_df[["Survived"]].copy()
@@ -189,9 +187,11 @@ def pre_proc_all():
         return train_df, target_df
     train_df, train_target_df = split_train_target(t_df, tf_surv=False)
     test_df,  test_target_df  = split_train_target(t_df, tf_surv=True)
-
+    
     return train_df, train_target_df, test_df, test_target_df
-
-
+    
+    
 if __name__=="__main__":
     train_df, train_target_df, test_df, test_target_df = pre_proc_all()
+
+    
